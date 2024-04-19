d0ca8db24deb7ea90f624bf088ee68d9ec66cf36
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2024-04-19 22:34:29 +0300

    annotated function return float

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -5,8 +5,9 @@ takes a list input_list of
 floats as argument and returns
 their sum as a float."""
 
+from typing import List
 
-def sum_list(input_list: float) -> float:
+def sum_list(input_list: List[float]) -> float:
     """annotated function that returns the sum of list.
     """
     sum = 0.0
